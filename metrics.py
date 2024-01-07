2caed931187665240948546dee9c74035223861b
Konrad Maciejczyk <conradmac97@gmail.com>
2024-01-07 01:15:32 +0100

    runet notebook redone

--- metrics.py
+++ metrics.py
@@ -4,4 +4,4 @@ def psnr(y_true, y_pred):
     return image.psnr(y_true, y_pred, max_val=1.0)
 
 def ssim(y_true,y_pred):
-    return tf.image.ssim(y_true,y_pred,1.0)
+    return image.ssim(y_true,y_pred,1.0)
