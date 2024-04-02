b78f4e827948b1c91e42f851d4f3f33ad583a33d
Robert Walery <robert.walery.99@gmail.com>
2024-04-02 10:54:24 +0200

    Code refactoring

--- Webapp/run.py
+++ Webapp/run.py
@@ -1,69 +1,69 @@
 import streamlit as st
-from PIL import Image
-from io import BytesIO
-import base64
-import os
+import tensorflow_hub as hub
 import numpy as np
 import tensorflow as tf
-import tensorflow_hub as hub
+import cv2
+import io
 
-def preprocess_image(image_path):
-  hr_image = tf.image.decode_image(tf.io.read_file(image_path))
-  if hr_image.shape[-1] == 4:
-    hr_image = hr_image[...,:-1]
-  hr_size = (tf.convert_to_tensor(hr_image.shape[:-1]) // 4) * 4
-  hr_image = tf.image.crop_to_bounding_box(hr_image, 0, 0, hr_size[0], hr_size[1])
-  hr_image = tf.cast(hr_image, tf.float32)
-  return tf.expand_dims(hr_image, 0)
-
-st.set_page_config(layout="wide", page_title="AI powered superresolution")
 SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
-MAX_FILE_SIZE = 5 * 1024 * 1024
-col1, col2 = st.columns(2)
 
+model = hub.load(SAVED_MODEL_PATH)
+
+st.set_page_config(page_title="Super-Resolution App", layout='wide')
+
+st.title('Welcome to our Super-Resolution App!')
+
+st.markdown("""Our project is a web application that allows for enhancing the 
+            resolution of images online. Utilizing advanced artificial 
+            intelligence technologies, our tool can significantly increase 
+            the quality of images, restoring their detail and sharpness.  
+            Discover the potential of artificial intelligence and make your 
+            images look better than ever before!  
+            [Github](https://github.com/Rol3ert99/super-resolution)  
+            * **Authors:** Robert Walery, Konrad Maciejczyk  
+            * **Python libraries:** tensorflow, opencv, streamlit, numpy
+            """)
 
 st.write("## Upload file")
 my_upload = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
+col1, col2 = st.columns(2)
+
+
+def read_image_bytes(image_bytes):
+    nparr = np.frombuffer(image_bytes.read(), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img
+
 
 if my_upload is not None:
-    if my_upload.size > MAX_FILE_SIZE:
-        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
-    else:
-        my_upload = Image.open(my_upload)
+    col1.header("Before")
+    col2.header("After")
+    col1.image(my_upload, use_column_width=True)
+    image = read_image_bytes(my_upload)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image.astype(np.float32)
+    image_resolution = image.shape[:2]
+    image = np.expand_dims(image, axis=0)
+    
+    hr_image = model(image)
+    hr_image = np.array(hr_image)
+    hr_image = hr_image[0,:,:,:]
+    hr_image = np.clip(hr_image, 0, 255)
+    hr_image = hr_image.astype(np.uint8)
+    hr_image_resolution = hr_image.shape[:2]
 
+    col2.image(hr_image, use_column_width=True)
 
-        my_upload.save('./image.png')
-        SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
-        IMAGE_PATH = './image.png'
+    col1.write("Original resolution: " + str(image_resolution[0]) + 'x' + str(image_resolution[1]))
 
-        hr_image = preprocess_image(IMAGE_PATH)
+    col2.write("Received resolution: " + str(hr_image_resolution[0]) + 'x' + str(hr_image_resolution[1]))
 
-        model = hub.load(SAVED_MODEL_PATH)
-        fake_image = model(hr_image)
-        fake_image = tf.squeeze(fake_image)
+    _, buffer = cv2.imencode('.png', cv2.cvtColor(hr_image, cv2.COLOR_RGB2BGR))
+    img_io = io.BytesIO(buffer)
+    col2.download_button(label='Click here to download',
+                         data=img_io,
+                         file_name='enhanced_image.png',
+                         mime='image/png')
 
-        fake_image = tf.squeeze(fake_image)
-        fake_image = np.asarray(fake_image)
-        fake_image = tf.clip_by_value(fake_image, 0, 255)
-        fake_image = Image.fromarray(tf.cast(fake_image, tf.uint8).numpy())
-
-        # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-        # model = RealESRGAN(device, scale=4)
-        # model.load_weights('weights/RealESRGAN_x4.pth', download=True)
-
-        # path_to_image = './image.png'
-        # image = Image.open(path_to_image).convert('RGB')
-
-        # fake_image = model.predict(image)
-
-        with st.container():
-          col1, col2 = st.columns(2)
-          with col1:
-            st.header("Before")
-            col1.image(my_upload, width=800)
-          
-          with col2:
-            st.header("After")
-            st.image(fake_image, width=800)
+    
