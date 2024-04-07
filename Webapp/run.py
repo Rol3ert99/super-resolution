1b754661f20e467adc3963a9245a2d587532727d
Robert Walery <robert.walery.99@gmail.com>
2024-04-07 16:30:10 +0200

    Code refactoring 2

--- Webapp/run.py
+++ Webapp/run.py
@@ -7,7 +7,31 @@ import io
 
 SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
 
-model = hub.load(SAVED_MODEL_PATH)
+
+def read_image_bytes(image_bytes):
+    nparr = np.frombuffer(image_bytes.read(), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img
+
+
+def generate_enhanced_image(upload):
+    model = hub.load(SAVED_MODEL_PATH)
+    image = read_image_bytes(upload)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image.astype(np.float32)
+    image_resolution = image.shape[:2]
+    image = np.expand_dims(image, axis=0)
+    
+    hr_image = model(image)
+    hr_image = np.array(hr_image)
+    hr_image = hr_image[0,:,:,:]
+    hr_image = np.clip(hr_image, 0, 255)
+    hr_image = hr_image.astype(np.uint8)
+    hr_image_resolution = hr_image.shape[:2]
+
+    return hr_image, image_resolution, hr_image_resolution
+
+
 
 st.set_page_config(page_title="Super-Resolution App", layout='wide')
 
@@ -29,37 +53,22 @@ my_upload = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
 col1, col2 = st.columns(2)
 
-
-def read_image_bytes(image_bytes):
-    nparr = np.frombuffer(image_bytes.read(), np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    return img
-
-
 if my_upload is not None:
     col1.header("Before")
     col2.header("After")
     col1.image(my_upload, use_column_width=True)
-    image = read_image_bytes(my_upload)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = image.astype(np.float32)
-    image_resolution = image.shape[:2]
-    image = np.expand_dims(image, axis=0)
-    
-    hr_image = model(image)
-    hr_image = np.array(hr_image)
-    hr_image = hr_image[0,:,:,:]
-    hr_image = np.clip(hr_image, 0, 255)
-    hr_image = hr_image.astype(np.uint8)
-    hr_image_resolution = hr_image.shape[:2]
 
-    col2.image(hr_image, use_column_width=True)
+    hr_img, img_resolution, hr_img_resolution = generate_enhanced_image(my_upload)
 
-    col1.write("Original resolution: " + str(image_resolution[0]) + 'x' + str(image_resolution[1]))
+    col2.image(hr_img, use_column_width=True)
 
-    col2.write("Received resolution: " + str(hr_image_resolution[0]) + 'x' + str(hr_image_resolution[1]))
+    col1.write("Original resolution: " + str(img_resolution[0]) + 'x' 
+               + str(img_resolution[1]))
 
-    _, buffer = cv2.imencode('.png', cv2.cvtColor(hr_image, cv2.COLOR_RGB2BGR))
+    col2.write("Received resolution: " + str(hr_img_resolution[0]) + 'x' 
+               + str(hr_img_resolution[1]))
+
+    _, buffer = cv2.imencode('.png', cv2.cvtColor(hr_img, cv2.COLOR_RGB2BGR))
     img_io = io.BytesIO(buffer)
     col2.download_button(label='Click here to download',
                          data=img_io,
